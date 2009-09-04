09d2eca0b6ee2aa52503ce3635ef0e246869faee
steffenw <steffenw@2090c879-ba7a-46ae-a66c-a00c6d59768f>
2009-09-04 05:49:59 +0000

    run the dc.. subs too
    git-svn-id: http://svn.code.sf.net/p/perl-gettext-oo/code@41 2090c879-ba7a-46ae-a66c-a00c6d59768f

---
+++ branches/category/example/02_gettext_struct_from_locale_po.pl
@@ -0,0 +1,152 @@
+#!perl -T
+
+use strict;
+use warnings;
+
+our $VERSION = 0;
+
+use Carp qw(croak);
+use English qw(-no_match_vars $OS_ERROR $EVAL_ERROR);
+require DBD::PO::Locale::PO;
+require Locale::TextDomain::OO;
+use Locale::Messages::AnyObject qw(set_object);
+require Locale::Messages::Struct;
+
+local $ENV{LANGUAGE} = 'de_DE';
+my $text_domain      = 'example_02';
+
+my $loc = Locale::TextDomain::OO->new(
+    gettext_package => 'Locale::Messages::AnyObject',
+    text_domain     => $text_domain,
+    search_dirs     => [qw(./LocaleData/)],
+);
+
+# find the database for the expected language
+# here fallback to 'de'
+my $file_path = $loc->get_file_path($text_domain, '.po');
+
+binmode STDOUT, ':encoding(utf-8)'
+    or croak "Binmode STDOUT\n$OS_ERROR";
+
+my $locale_po = DBD::PO::Locale::PO->new();
+my $array_ref = $locale_po->load_file_asarray("$file_path/$text_domain.po");
+
+# header
+my $header = ( shift @{$array_ref} )->msgstr();
+my ($plural_forms) = $header=~  m{^ Plural-Forms: \s (.*) \n}xms;
+
+# convert array_ref of objects to array_ref of hashes
+for my $entry ( @{$array_ref} ) {
+    $entry = {
+        msgctxt      => scalar $entry->msgctxt(),
+        msgid        => scalar $entry->msgid(),
+        msgid_plural => scalar $entry->msgid_plural(),
+        msgstr       => scalar $entry->msgstr(),
+        do {
+            my $msgstr_n = $entry->msgstr_n();
+            $msgstr_n
+            ? (
+                map {
+                    ( "msgstr_$_" => $msgstr_n->{$_} );
+                } keys %{$msgstr_n}
+            )
+            : ();
+        },
+    };
+}
+
+# build the struct and bind the struct as object to the text domain
+my %struct = (
+    $text_domain => {
+        plural_ref => $loc->get_function_ref_plural($plural_forms),
+        array_ref  => $array_ref,
+    },
+);
+set_object($text_domain => Locale::Messages::Struct->new(\%struct));
+
+# run all translations
+() = print map {"$_\n"}
+    $loc->__(
+        'This is a text.',
+    ),
+    $loc->__x(
+        '{name} is programming {language}.',
+        name     => 'Steffen',
+        language => 'Perl',
+    ),
+    $loc->__n(
+        'Singular',
+        'Plural',
+        1,
+    ),
+    $loc->__n(
+        'Singular',
+        'Plural',
+        2,
+    ),
+    $loc->__nx(
+        '{num} shelf',
+        '{num} shelves',
+        1,
+        num => 1,
+    ),
+    $loc->__nx(
+        '{num} shelf',
+        '{num} shelves',
+        2,
+        num => 2,
+    ),
+    $loc->__p(
+        'maskulin',
+        'Dear',
+    ),
+    $loc->__px(
+        'maskulin',
+        'Dear {name}',
+        name => 'Winkler',
+    ),
+    $loc->__np(
+        'better',
+        'shelf',
+        'shelves',
+        1,
+    ),
+    $loc->__np(
+        'better',
+        'shelf',
+        'shelves',
+        2,
+    ),
+    $loc->__npx(
+        'better',
+        '{num} shelf',
+        '{num} shelves',
+        1,
+        num => 1,
+    ),
+    $loc->__npx(
+        'better',
+        '{num} shelf',
+        '{num} shelves',
+        2,
+        num => 2,
+    );
+
+# $Id$
+
+__END__
+
+Output:
+
+Das ist ein Text.
+Steffen programmiert Perl.
+Einzahl
+Mehrzahl
+1 Regal
+2 Regale
+Sehr geehrter Herr
+Sehr geehrter Herr Winkler
+gutes Regal
+gute Regale
+1 gutes Regal
+2 gute Regale
---
+++ branches/category/lib/Locale/Messages/AnyObject.pm
@@ -0,0 +1,198 @@
+package Locale::Messages::AnyObject;
+
+use strict;
+use warnings;
+
+use version; our $VERSION = qv('0.01');
+
+use Carp qw(croak);
+use Perl6::Export::Attrs;
+
+our %object_of; ## no critic (PackageVars)
+
+sub set_object :Export(:DEFAULT) {
+    my ($text_domain, $object) = @_;
+
+    defined $text_domain
+        or croak 'Text domain is undefined';
+    length $text_domain
+        or croak 'Text domain is empty';
+
+    return $object_of{$text_domain} = $object;
+}
+
+sub get_object :Export() {
+    my $text_domain = shift;
+
+    defined $text_domain
+        or croak 'Text domain is undefined';
+    length $text_domain
+        or croak 'Text domain is empty';
+
+    exists $object_of{$text_domain}
+        or croak
+            'Unknown text domain, call set_object'
+            . ' to connect the text domain and the object';
+
+    return $object_of{$text_domain};
+}
+
+sub dgettext :Export() {
+    my ($text_domain, $msgid) = @_;
+
+    return get_object($text_domain)->dgettext(
+        $text_domain,
+        $msgid,
+    );
+}
+
+sub dngettext :Export() {
+    my ($text_domain, $msgid, $msgid_plural, $count) = @_;
+
+    return get_object($text_domain)->dngettext(
+        $text_domain,
+        $msgid,
+        $msgid_plural,
+        $count,
+    );
+}
+
+sub dpgettext :Export() {
+    my ($text_domain, $msgctxt, $msgid) = @_;
+
+    return get_object($text_domain)->dpgettext(
+        $text_domain,
+        $msgctxt,
+        $msgid,
+    );
+}
+
+sub dnpgettext :Export() {
+    my ($text_domain, $msgctxt, $msgid, $msgid_plural, $count) = @_;
+
+    return get_object($text_domain)->dnpgettext(
+        $text_domain,
+        $msgctxt,
+        $msgid,
+        $msgid_plural,
+        $count,
+    );
+}
+
+1;
+
+__END__
+
+=head1 NAME
+
+Locale::Messages::AnyObject - Perl Interface to use gettext but not mo-files
+
+$Id$
+
+$HeadURL$
+
+=head1 VERSION
+
+0.01
+
+=head1 DESCRIPTION
+
+This module maps the subroutine call for
+dgettext, dngettext, dpgettext and dnpgettext
+back to object method calls with the same name.
+
+So the module L<Locale::Text::Domain::OO>
+can accept this module instead of module L<Locale::Messages>.
+
+This module allows to connect your own object-oriented modules
+if your message catalog is not an mo-file.
+
+L<Locale::Messages::Struct> ist such one.
+The idea is to read the database information into a data structure
+for fast access.
+
+=head1 SYNOPSIS
+
+    use Locale::Messages::AnyObject;
+
+=head1 SUBROUTINES/METHODS
+
+=head2 sub set_object
+
+This sub is exported by default.
+
+    set_object($text_domain, $object);
+
+=head2 sub get_object
+
+    $object = set_object($text_domain);
+
+=head2 sub dgettext
+
+    $translation = dgettext($text_domain, $msgid);
+
+=head2 sub dngettext
+
+    $translation = dngettext($text_domain, $msgid, $msgid_plural, $count);
+
+=head2 sub dpgettext
+
+    $translation = dpgettext($text_domain, $msgctxt, $msgid);
+
+=head2 sub dnpgettext
+
+    $translation = dnpgettext($text_domain, $msgctxt, $msgid, $msgid_plural, $count);
+
+=head1 EXAMPLE
+
+Inside of this Distribution is a directory named example.
+Run this *.pl files.
+
+=head1 DIAGNOSTICS
+
+A text domain is a non empty string.
+
+ Text domain is undefined
+
+ Text doamin is empty
+
+Method get_object could not find the object.
+
+ Unknown text domain, call set_object to connect the text domain and the object
+
+=head1 CONFIGURATION AND ENVIRONMENT
+
+none
+
+=head1 DEPENDENCIES
+
+L<Perl6::Export::Attrs>
+
+=head1 INCOMPATIBILITIES
+
+not known
+
+=head1 BUGS AND LIMITATIONS
+
+none
+
+=head1 SEE ALSO
+
+L<Locale::TextDoamin::OO>
+
+=head1 AUTHOR
+
+Steffen Winkler
+
+=head1 LICENSE AND COPYRIGHT
+
+Copyright (c) 2009,
+Steffen Winkler
+C<< <steffenw at cpan.org> >>.
+All rights reserved.
+
+This module is free software;
+you can redistribute it and/or modify it
+under the same terms as Perl itself.
+
+=cut
