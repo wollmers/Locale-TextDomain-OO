3095d9a91e21eff062f4edd0c192bd3abe23e764
steffenw <steffenw@2090c879-ba7a-46ae-a66c-a00c6d59768f>
2009-09-15 08:12:34 +0000

    rename examples
    git-svn-id: http://svn.code.sf.net/p/perl-gettext-oo/code@76 2090c879-ba7a-46ae-a66c-a00c6d59768f

---
+++ trunk/example/01_locale_textdomian_utf-8.pl
@@ -0,0 +1,43 @@
+#!perl -T
+
+use strict;
+use warnings;
+use utf8;
+
+our $VERSION = 0;
+
+use Carp qw(croak);
+use English qw(-no_match_vars $OS_ERROR);
+use Encode qw(encode_utf8 decode_utf8);
+require Locale::TextDomain;
+use Locale::Messages qw(bind_textdomain_filter);
+
+local $ENV{LANGUAGE} = 'ru';
+my $text_domain      = 'test';
+
+# bind text domain
+Locale::TextDomain->import( $text_domain, qw(./LocaleData) );
+
+# bind output_filter
+bind_textdomain_filter($text_domain, \&decode_utf8);
+
+binmode STDOUT, ':encoding(utf-8)'
+    or croak "Binmode STDOUT\n$OS_ERROR";
+
+# run all translations
+() = print map {"$_\n"}
+    __(
+        'book',
+    ),
+    __(
+        encode_utf8('§ book'),
+    );
+
+# $Id$
+
+__END__
+
+Output:
+
+книга
+§ книга
