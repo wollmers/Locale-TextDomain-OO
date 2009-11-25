796fde726ad6c6c94ac6f484927b6aaff66e0700
steffenw <steffenw@2090c879-ba7a-46ae-a66c-a00c6d59768f>
2009-11-25 20:59:06 +0000

    rename performance tests add a new performance test
    git-svn-id: http://svn.code.sf.net/p/perl-gettext-oo/code@121 2090c879-ba7a-46ae-a66c-a00c6d59768f

---
+++ 11_performance_ltd_mo.pl
@@ -0,0 +1,100 @@
+#!perl -T
+
+use strict;
+use warnings;
+
+our $VERSION = 0;
+
+require Locale::TextDomain;
+
+local $ENV{LANGUAGE} = 'de_DE';
+my $text_domain      = 'example';
+
+Locale::TextDomain::->import( $text_domain => qw(./LocaleData/) );
+
+# run all translations
+() =
+    __(
+        'This is a text.',
+    ),
+    __x(
+        '{name} is programming {language}.',
+        name     => 'Steffen',
+        language => 'Perl',
+    ),
+    __n(
+        'Singular',
+        'Plural',
+        1,
+    ),
+    __n(
+        'Singular',
+        'Plural',
+        2,
+    ),
+    __nx(
+        '{num} shelf',
+        '{num} shelves',
+        1,
+        num => 1,
+    ),
+    __nx(
+        '{num} shelf',
+        '{num} shelves',
+        2,
+        num => 2,
+    ),
+    __p(
+        'maskulin',
+        'Dear',
+    ),
+    __px(
+        'maskulin',
+        'Dear {name}',
+        name => 'Winkler',
+    ),
+    __np(
+        'better',
+        'shelf',
+        'shelves',
+        1,
+    ),
+    __np(
+        'better',
+        'shelf',
+        'shelves',
+        2,
+    ),
+    __npx(
+        'better',
+        '{num} shelf',
+        '{num} shelves',
+        1,
+        num => 1,
+    ),
+    __npx(
+        'better',
+        '{num} shelf',
+        '{num} shelves',
+        2,
+        num => 2,
+    ) for (1 .. 1000);
+
+# $Id: 01_gettext_mo.pl 15 2009-08-30 11:13:33Z steffenw $
+
+__END__
+
+Output:
+
+Das ist ein Text.
+Steffen programmiert Perl.
+Einzahl
+Mehrzahl
+1 Regal
+2 Regale
+Sehr geehrter Herr
+Sehr geehrter Herr Winkler
+gutes Regal
+gute Regale
+1 gutes Regal
+2 gute Regale
--- performance-Locale-TextDomain-OO.bat
+++ performance-Locale-TextDomain-OO.bat
@@ -1,21 +1,28 @@
 @echo off
 
-perl -MFile::Copy -e "copy qw(11_performance_gettext_mo.pl trunk/example/)"
-perl -MFile::Copy -e "copy qw(12_performance_gettext_struct_from_locale_po.pl trunk/example/)"
+perl -MFile::Copy -e "copy qw(11_performance_ltd_mo.pl                       trunk/example/)"
+perl -MFile::Copy -e "copy qw(12_performance_ltd_oo_mo.pl                    trunk/example/)"
+perl -MFile::Copy -e "copy qw(13_performance_ltd_oo_struct_from_locale_po.pl trunk/example/)"
 cd trunk/example/
 
-perl -T -I../lib -d:DProf 11_performance_gettext_mo.pl
+perl -T -I../lib -d:DProf 11_performance_ltd_mo.pl
+rem perl -T -I../lib -MDevel::Profiler 10_performance_gettext_mo.pl
+
+call dprofpp
+rem call dprofpp -T
+
+perl -T -I../lib -d:DProf 12_performance_ltd_oo_mo.pl
 rem perl -T -I../lib -MDevel::Profiler 11_performance_gettext_mo.pl
 
 call dprofpp
 rem call dprofpp -T
 
-perl -T -I../lib -d:DProf 12_performance_gettext_struct_from_locale_po.pl
+perl -T -I../lib -d:DProf 13_performance_ltd_oo_struct_from_locale_po.pl
 rem perl -T -I../lib -MDevel::Profiler 12_performance_gettext_struct_from_locale_po.pl
 
 call dprofpp
 rem call dprofpp -T
 
-perl -e "unlink qw(11_performance_gettext_mo.pl delete 12_performance_gettext_struct_from_locale_po.pl tmon.out)"
+perl -e "unlink qw(11_performance_ltd_mo.pl 12_performance_ltd_oo_mo.pl 13_performance_ltd_oo_struct_from_locale_po.pl tmon.out)"
 
 pause
