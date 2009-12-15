053ac0fea8d7f514f1e36ff057bd7242cdfafd84
steffenw <steffenw@2090c879-ba7a-46ae-a66c-a00c6d59768f>
2009-12-15 06:28:20 +0000

    check in during development
    git-svn-id: http://svn.code.sf.net/p/perl-gettext-oo/code@196 2090c879-ba7a-46ae-a66c-a00c6d59768f

--- trunk/example/14_N__.pl
+++ trunk/example/14_N__.pl
@@ -7,58 +7,84 @@ our $VERSION = 0;
 
 require Locale::TextDomain::OO;
 
-my $loc = Locale::TextDomain::OO->new();
+local $ENV{LANGUAGE}
+    = Locale::TextDomain::OO
+    ->get_default_language_detect()
+    ->('de_DE');
+my $text_domain = 'example';
 
-# run all translations
-() = print map {"$_\n"}
-    $loc->N__(
-        'This is a text.',
-    ),
-    q{},
-    $loc->N__x(
-        '{name} is programming {language}.',
-        name     => 'Steffen',
-        language => 'Perl',
-    ),
-    q{},
-    $loc->N__n(
-        'Singular',
-        'Plural',
-        1,
-    ),
-    q{},
-    $loc->N__nx(
-        '{num} shelf',
-        '{num} shelves',
-        1,
-        num => 1,
-    ),
-    q{},
-    $loc->N__p(
-        'maskulin',
-        'Dear',
-    ),
-    q{},
-    $loc->N__px(
-        'maskulin',
-        'Dear {name}',
-        name => 'Winkler',
-    ),
-    q{},
-    $loc->N__np(
-        'better',
-        'shelf',
-        'shelves',
-        1,
-    ),
-    q{},
-    $loc->N__npx(
-        'better',
-        '{num} shelf',
-        '{num} shelves',
-        1,
-        num => 1,
-    );
+my $loc = Locale::TextDomain::OO->new(
+    text_domain => $text_domain,
+    search_dirs => [qw(./LocaleData/)],
+);
+
+# Put all data for the translation into a structure.
+# That allows the extractor to find all the phrases.
+my @extractable_data = (
+    '' => [
+        $loc->N__(
+            'This is a text.',
+        )
+    ],
+    x => [
+        $loc->N__x(
+            '{name} is programming {language}.',
+            name     => 'Steffen',
+            language => 'Perl',
+        )
+    ],
+    n => [
+        $loc->N__n(
+            'Singular',
+            'Plural',
+            1,
+        )
+    ],
+    nx => [
+        $loc->N__nx(
+            '{num} shelf',
+            '{num} shelves',
+            1,
+            num => 1,
+        )
+    ],
+    p => [
+        $loc->N__p(
+            'maskulin',
+            'Dear',
+        )
+    ],
+    px => [
+        $loc->N__px(
+            'maskulin',
+            'Dear {name}',
+            name => 'Winkler',
+        )
+    ],
+    np => [
+        $loc->N__np(
+            'better',
+            'shelf',
+            'shelves',
+            1,
+        )
+    ],
+    npx => [
+        $loc->N__npx(
+            'better',
+            '{num} shelf',
+            '{num} shelves',
+            1,
+            num => 1,
+        )
+    ],
+);
+
+# Do any complex things and run the translations later.
+while (my ($method_suffix, $array_ref) = splice @extractable_Data, 0, 2) {
+    my $method = "__$mtheod_suffix;
+    $loc->$method( @{$array_ref );
+}
 
 # $Id$
 
@@ -66,40 +92,15 @@ __END__
 
 Output:
 
-This is a text.
-
-{name} is programming {language}.
-name
-Steffen
-language
-Perl
-
-Singular
-Plural
-1
-
-{num} shelf
-{num} shelves
-1
-num
-1
-
-maskulin
-Dear
-
-maskulin
-Dear {name}
-name
-Winkler
-
-better
-shelf
-shelves
-1
-
-better
-{num} shelf
-{num} shelves
-1
-num
-1
+Das ist ein Text.
+Steffen programmiert Perl.
+Einzahl
+Mehrzahl
+1 Regal
+2 Regale
+Sehr geehrter Herr
+Sehr geehrter Herr Winkler
+gutes Regal
+gute Regale
+1 gutes Regal
+2 gute Regale
