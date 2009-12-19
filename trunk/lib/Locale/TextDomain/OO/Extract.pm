2591e18c3320cbf66bfa5b1a4381f5c07c7e3a3a
steffenw <steffenw@2090c879-ba7a-46ae-a66c-a00c6d59768f>
2009-12-19 16:35:53 +0000

    check in during development
    git-svn-id: http://svn.code.sf.net/p/perl-gettext-oo/code@207 2090c879-ba7a-46ae-a66c-a00c6d59768f

--- trunk/lib/Locale/TextDomain/OO/Extract.pm
+++ trunk/lib/Locale/TextDomain/OO/Extract.pm
@@ -7,7 +7,7 @@ use version; our $VERSION = qv('0.04');
 
 use Carp qw(croak);
 use English qw(-no_match_vars $OS_ERROR $INPUT_RECORD_SEPARATOR);
-use Clone qw(clone);
+use Clone qw(clone); # clones not recursive
 require DBI;
 require DBD::PO; DBD::PO->init( qw(:plural) );
 
@@ -23,10 +23,12 @@ my $context_rule
         qr{'}xms,
         qr{( (?: \\\\ \\\\ | \\\\ ' | [^'] )+ )}xms,
         qr{'}xms,
+        'END',
     ];
-my $komma_rule               = qr{,}xms;
+my $komma_rule = qr{,}xms;
 my $optional_whitespace_rule = qr{\s*}xms;
-my $perl_start_rule          = qr{__ n?p?x? \(}xms;
+
+my $perl_start_rule = qr{__ n?p?x? \(}xms;
 my $perl_rules = [
     [
         qr{__ (x?) \(}xms,
@@ -71,31 +73,60 @@ sub new {
 
     my $self = bless {}, $class;
 
-    $self->_set_pot_dir(
-        defined $init{pot_dir}
-        ? delete $init{pot_dir}
-        : q{.}
-    );
+    # prepare the file and the encoding
     $self->_set_preprocess(
         ( defined $init{preprocess} && ref $init{preprocess} eq 'CODE' )
         ? delete $init{preprocess}
         : $perl_remove_pod
     );
+
+    # how to find such lines
     $self->_set_start_rule(
         defined $init{start_rule}
         ? delete $init{start_rule}
         : $perl_start_rule
     );
+
+    # how to find the parameters
     $self->_set_rules(
         defined $init{rules}
         ? delete $init{rules}
         : $perl_rules
     );
 
+    # debug output for other rules than perl
+    $self->_set_is_debug(
+        defined $init{is_debug}
+        ? delete $init{is_debug}
+        : ()
+    );
+
+    # where to store the pot file
+    if ( defined $init{pot_dir} ) {
+        $self->$self->_set_pot_dir( delete $init{pot_dir} );
+    }
+
+    # how to store the pot file
+    if ( defined $init{pot_charset} ) {
+        $self->$self->_set_pot_charset( delete $init{pot_charset} );
+    }
+
+    # error
+    my $keys = join ', ', keys %init;
+    if ($keys) {
+        croak "Unknown parameter: $keys";
+    }
+
     return $self;
 }
 
-for my $name ( qw(pot_dir preprocess start_rule rules content_ref references_ref) ) {
+my @names = qw(
+    preprocess start_rule rules is_debug
+    pot_dir pot_charset
+    content_ref references_ref
+);
+
+for my $name (@names) {
     no strict qw(refs);       ## no critic (NoStrict)
     no warnings qw(redefine); ## no critic (NoWarnings)
     *{"_set_$name"} = sub {
@@ -108,6 +139,28 @@ for my $name ( qw(pot_dir preprocess start_rule rules content_ref references_ref
     *{"_get_$name"} = sub {
         return shift->{$name};
     };
+}
+
+sub debug {
+    my ($self, $message) = @_;
+
+    defined $message
+        or return $self->debug('undef');
+    print STDERR "\n# $message";
+
+    return $self;
+}
+
+sub _debug {
+    my ($self, @messages) = @_;
+
+    $self->_get_is_debug()
+        or return $self;
+    for my $message (@messages) {
+        $self->debug($message);
+    }
+
+    return $self;
 }
 
 sub _parse_pos {
@@ -133,41 +186,80 @@ sub _parse_rules {
     for my $reference ( @{ $self->_get_references_ref() } ) {
         my $rules = clone $self->_get_rules();
         my $pos   = $reference->{start_pos};
+        $self->_debug("Starting at pos $pos.");
         my (@parent_rules, @parent_pos);
         RULE: {
             my $rule = shift @{$rules};
-            # goto parent
-            if (! $rule && @parent_rules) {
-                $rules = pop @parent_rules;
-                $pos   = pop @parent_pos;
-                redo RULE;
+            # goto parent an find next alternative
+            if (! $rule) {
+                $self->_debug('No more rules found.');
+                if (@parent_rules) {
+                    $rules = pop @parent_rules;
+                    $pos   = pop @parent_pos;
+                    $self->_debug(
+                        'Should try next alternative.',
+                        'Going back to parent.',
+                        "Current pos is $pos.",
+                    );
+                    redo RULE;
+                }
+                last RULE;
             }
-            $rule
-               or last RULE;
             # goto child
             if ( ref $rule eq 'ARRAY' ) {
                 push @parent_rules, $rules;
                 push @parent_pos,   $pos;
-                $rules = $rule;
+                $rules = clone $rule;
+                $self->_debug('Going to child.');
+                redo RULE;
+            }
+            # end of child, goto parent
+            elsif ( $rule eq 'END') {
+                $rules = pop @parent_rules;
+                pop @parent_pos;
+                $self->_debug(
+                    'End of child detected.',
+                    'Going back to parent.',
+                    "Current pos is $pos.",
+                );
                 redo RULE;
             }
             pos ${ $content_ref } = $pos;
+            $self->_debug("Set the current pos to $pos.");
             my $has_matched
                 = my ($match, @result)
                 = ${$content_ref} =~ m{\G ($rule)}xms;
             if ($has_matched) {
                 push @{ $reference->{parameter} }, @result;
                 $pos += length $match;
-print STDERR "\n+++ $rule";
+                $self->_debug(
+                    qq{Rule $rule has matched:},
+                    ( split m{\n}xms, $match ),
+                    "The current pos is $pos.",
+                );
             }
             else {
                 $rules = pop @parent_rules;
                 $pos   = pop @parent_pos;
-print STDERR "\n--- $rule";
+                $self->_debug(
+                    "Rule $rule has not matched.",
+                    'Going back to parent.',
+                    "The current pos is $pos.",
+                );
             }
             redo RULE;
         }
     }
+
+    return $self;
+}
+
+sub _cleanup {
+    my $self = shift;
+
+    my $references_ref = $self->_get_references_ref();
+    my $index = 0;
+    @{$references_ref} = grep { exists $_->{parameter} } @{$references_ref};
 
     return $self;
 }
@@ -185,17 +277,134 @@ sub _calculate_reference {
     return $self;
 }
 
-sub extract {
-    my ($self, $file_name_or_open_handle) = @_;
+sub _calculate_pot_data {
+    my ($self, $file_name) = @_;
 
-    my ($file_name, $file_handle);
-    if (ref $file_name_or_open_handle) {
-        $file_name
-            = $file_handle
-            = $file_name_or_open_handle;
+    for my $reference ( @{ $self->_get_references_ref() } ) {
+        my @parameter = @{ delete $reference->{parameter} };
+        $reference->{pot_data} = {
+            reference    => "$file_name:$reference->{line_number}",
+            msgctxt      => $parameter[0] =~ m{p}xms
+                            ? scalar shift @parameter
+                            : undef,
+            msgid        => scalar shift @parameter,
+            msgid_plural => scalar shift @parameter,
+        };
     }
-    else {
-        $file_name = $file_name_or_open_handle;
+
+    return $self;
+}
+
+sub _store_pot_file {
+    my ($self, $file_name) = @_;
+
+    # create a new pot file
+    my $dbh = DBI->connect(
+        'DBI:PO:'
+        . (
+            join q{;}, (
+                (
+                    defined $self->_get_pot_dir()
+                    ? join q{=}, 'f_dir', $self->_get_pot_dir()
+                    : ()
+                ),
+                (
+                    defined $self->_get_pot_charset()
+                    ? join q{=}, 'po_charset', $self->_get_pot_charset()
+                    : ()
+                ),
+            )
+        ),
+        undef,
+        undef,
+        {RaiseError => 1},
+    );
+    $dbh->{po_tables}->{pot} = {file => "$file_name.pot"};
+    $dbh->do('DROP TABLE IF EXISTS pot');
+    $dbh->do(<<'EO_SQL');
+        CREATE TABLE pot (
+            reference    VARCHAR,
+            msgctxt      VARCHAR,
+            msgid        VARCHAR,
+            msgid_plural VARCHAR
+        )
+EO_SQL
+
+    # write the header
+    my $header_msgstr = $dbh->func(
+        { 'Plural-Forms' => 'nplurals=2; plural=n != 1;' },
+        'build_header_msgstr',
+    );
+    $dbh->do(<<'EO_SQL', undef, $header_msgstr);
+        INSERT INTO pot
+        (msgstr)
+        VALUES (?)
+EO_SQL
+
+    # to check if the entry is known
+    my $sth_select = $dbh->prepare(<<'EO_SQL');
+        SELECT reference
+        FROM pot
+        WHERE
+            msgctxt=?
+            AND msgid=?
+            AND msgid_plural=?
+EO_SQL
+
+    # to insert a new entry
+    my $sth_insert = $dbh->prepare(<<'EO_SQL');
+        INSERT INTO pot
+        (reference, msgctxt, msgid, msgid_plural)
+        VALUES (?, ?, ?, ?)
+EO_SQL
+
+    # to add the next reference to a known entry
+    my $sth_update = $dbh->prepare(<<'EO_SQL');
+        UPDATE pot
+        SET reference=?
+        WHERE
+            msgctxt=?
+            AND msgid=?
+            AND msgid_plural=?
+EO_SQL
+
+    # write entrys
+    for my $reference ( @{ $self->_get_references_ref() } ) {
+        my $entry = $reference->{pot_data};
+        $sth_select->execute(
+            @{$entry}{ qw(msgctxt msgid msgid_plural) },
+        );
+        my ($reference) = $sth_select->fetchrow_array();
+        if ($reference && length $reference) {
+            # Concat with the po_separator. The default is "\n".
+            $reference = "$reference\n$entry->{reference}";
+            $sth_update->execute(
+                $reference,
+                @{$entry}{ qw(msgctxt msgid msgid_plural) },
+            );
+        }
+        else {
+            $sth_insert->execute(
+                @{$entry}{ qw(reference msgctxt msgid msgid_plural) },
+            );
+        }
+    }
+
+    # all finished
+    for ($sth_select, $sth_insert, $sth_update) {
+        $_->finish();
+    }
+    $dbh->disconnect();
+
+    return $self;
+}
+
+sub extract {
+    my ($self, $file_name, $file_handle) = @_;
+
+    defined $file_name
+        or croak 'No file name given';
+    if (! ref $file_handle) {
         open $file_handle, '<', $file_name
             or croak "Can not open file $file_name\n$OS_ERROR";
     }
@@ -207,127 +416,13 @@ sub extract {
     $self->_get_preprocess()->( $self->_get_content_ref() );
     $self->_parse_pos();
     $self->_parse_rules();
+    $self->_cleanup();
     $self->_calculate_reference();
-use Data::Dumper; die Dumper $self->_get_references_ref();
-    $self->_store_pot();
+    $self->_calculate_pot_data($file_name);
+    $self->_store_pot_file($file_name);
 
     return $self;
 }
-
-1;
-
-__END__
-
-        # found
-        $has_matched ||= $text =~ s{$rule}{}xms; # delete found string
-        my $format = shift @result || q{};
-        push @pot_data, {
-            # where found
-            reference    => __FILE__ . ":$line_number",
-            # optional context
-            msgctxt      => $format =~ m{p}xms
-                            ? shift @result
-                            # DBD::PO fetch NULL as empty string
-                            : q{},
-            # the original text
-            msgid        => shift @result,
-            # optional origninal text in the plural form
-            msgid_plural => $format =~ m{n}xms
-                            ? shift @result
-                            # DBD::PO fetch NULL as empty string
-                           : q{},
-        };
-    }
-    if ($has_matched) {
-        @lines[($line_number - 1) .. $#lines] = split m{\n}xms, $text;
-    }
-    ++$line_number;
-}
-
-# Deleting the pot file if this exists,
-# so that this example can be going on repeatedly.
-unlink 'extract.pot';
-
-# create a new pot file
-my $dbh = DBI->connect(
-    'DBI:PO:po_charset=UTF-8',
-    undef,
-    undef,
-    {RaiseError => 1},
-);
-$dbh->do(<<'EO_SQL');
-     CREATE TABLE extract.pot (
-         reference    VARCHAR,
-         msgctxt      VARCHAR,
-         msgid        VARCHAR,
-         msgid_plural VARCHAR
-     )
-EO_SQL
-
-# write the header
-my $header_msgstr = $dbh->func(
-    { 'Plural-Forms' => 'nplurals=2; plural=n != 1;' },
-    'build_header_msgstr',
-);
-$dbh->do(<<'EO_SQL', undef, $header_msgstr);
-     INSERT INTO extract.pot
-     (msgstr)
-     VALUES (?)
-EO_SQL
-
-# to check if the entry is known
-my $sth_select = $dbh->prepare(<<'EO_SQL');
-     SELECT reference
-     FROM extract.pot
-     WHERE
-         msgctxt=?
-         AND msgid=?
-         AND msgid_plural=?
-EO_SQL
-
-# to insert a new entry
-my $sth_insert = $dbh->prepare(<<'EO_SQL');
-     INSERT INTO extract.pot
-     (reference, msgctxt, msgid, msgid_plural)
-     VALUES (?, ?, ?, ?)
-EO_SQL
-
-# to add the next reference to a known entry
-my $sth_update = $dbh->prepare(<<'EO_SQL');
-     UPDATE extract.pot
-     SET reference=?
-     WHERE
-         msgctxt=?
-         AND msgid=?
-         AND msgid_plural=?
-EO_SQL
-
-# write entrys
-for my $entry (@pot_data) {
-    $sth_select->execute(
-            @{$entry}{ qw(msgctxt msgid msgid_plural) },
-    );
-    my ($reference) = $sth_select->fetchrow_array();
-    if ($reference && length $reference) {
-        # Concat with the po_separator. The default is "\n".
-        $reference = "$reference\n$entry->{reference}";
-        $sth_update->execute(
-            $reference,
-            @{$entry}{ qw(msgctxt msgid msgid_plural) },
-        );
-    }
-    else {
-        $sth_insert->execute(
-            @{$entry}{ qw(reference msgctxt msgid msgid_plural) },
-        );
-    }
-}
-
-# all finished
-for ($sth_select, $sth_insert, $sth_update) {
-    $_->finish();
-}
-$dbh->disconnect();
 
 1;
 
--- trunk/t/61_extract_perl.t
+++ trunk/t/61_extract_perl.t
@@ -3,7 +3,7 @@
 use strict;
 use warnings;
 
-use Test::More tests => 15 + 1;
+use Test::More tests => 2 + 1;
 use Test::NoWarnings;
 use Test::Exception;
 
