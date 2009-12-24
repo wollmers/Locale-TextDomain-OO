316d7bedc8401251191a3cf93705306eabcb1519
steffenw <steffenw@2090c879-ba7a-46ae-a66c-a00c6d59768f>
2009-12-24 12:06:29 +0000

    add all combinations
    git-svn-id: http://svn.code.sf.net/p/perl-gettext-oo/code@223 2090c879-ba7a-46ae-a66c-a00c6d59768f

--- trunk/example/files_to_parse/javascript.js
+++ trunk/example/files_to_parse/javascript.js
@@ -20,3 +20,28 @@ alert( pgettext( "Verb: To View", "View" ) );
 alert( pgettext( "Noun: A View", "View"  ) );
 var count = 14;
 Gettext.strargs( gt.ngettext('one banana', '%1 bananas', count), [count] );
+
+
+// do not find _('error 1'); _("error 2");
+
+/* 
+    do not find
+    _('error 3');
+    _("error 4");
+*/
+
+// this are all combinations
+
+gettext(     'MSGID 1' ) 
+_      (     'MSGID 2' )
+ngettext(    'MSGID 3', 'MSGID_PLURAL', COUNT ) 
+pgettext(    'MSGCTXT', 'MSGID 4' ) 
+npgettext(   'MSGCTXT', 'MSGID 5', 'MSGID_PLURAL', COUNT ) 
+dgettext(    'TEXTDOMAIN', 'MSGID 6' ) 
+dcgettext(   'TEXTDOMAIN', 'MSGID 7', 'CATEGORY' ) 
+dngettext(   'TEXTDOMAIN', 'MSGID 8', 'MSGID_PLURAL', COUNT ) 
+dcngettext(  'TEXTDOMAIN', 'MSGID 9', 'MSGID_PLURAL', COUNT, 'CATEGORY' ) 
+dpgettext(   'TEXTDOMAIN', 'MSGCTXT', 'MSGID 10' ) 
+dcpgettext(  'TEXTDOMAIN', 'MSGCTXT', 'MSGID 11', 'CATEGORY' ) 
+dnpgettext(  'TEXTDOMAIN', 'MSGCTXT', 'MSGID 12', 'MSGID_PLURAL', COUNT ) 
+dcnpgettext( 'TEXTDOMAIN', 'MSGCTXT', 'MSGID 13', 'MSGID_PLURAL', COUNT, 'CATEGORY' )
