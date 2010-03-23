efb67ec7a625faf6f2998d216e62fce91db4a636
steffenw <steffenw@2090c879-ba7a-46ae-a66c-a00c6d59768f>
2010-03-23 06:24:42 +0000

    use moose
    git-svn-id: http://svn.code.sf.net/p/perl-gettext-oo/code@324 2090c879-ba7a-46ae-a66c-a00c6d59768f

---
+++ extract/branches/moose/lib/Locale/TextDomain/OO/Extract.pm
@@ -0,0 +1,665 @@
+package Locale::TextDomain::OO::Extract;
+
+use strict;
+use warnings;
+
+our $VERSION = '0.05';
+
+use Carp qw(croak);
+use English qw(-no_match_vars $OS_ERROR $INPUT_RECORD_SEPARATOR);
+use Clone qw(clone); # clones not recursive
+use DBI ();
+use DBD::PO ();
+
+sub init {
+    my (undef, @more) = @_;
+
+    return DBD::PO->init(@more);
+}
+
+sub new {
+    my ($class, %init) = @_;
+
+    my $self = bless {}, $class;
+
+    # prepare the file and the encoding
+    if ( ref $init{preprocess_code} eq 'CODE' ) {
+        $self->_set_preprocess_code( delete $init{preprocess_code} );
+    }
+
+    # how to find such lines
+    if ( defined $init{start_rule} ) {
+        $self->_set_start_rule( delete $init{start_rule} );
+    }
+
+    # how to find the parameters
+    if ( ref $init{rules} eq 'ARRAY' ) {
+        $self->_set_rules( delete $init{rules} );
+    }
+
+    # debug output for other rules than perl
+    $self->_set_run_debug( delete $init{run_debug} );
+
+    # how to map the parameters to pot file
+    if ( ref $init{parameter_mapping_code} eq 'CODE' ) {
+        $self->_set_parameter_mapping_code(
+            delete $init{parameter_mapping_code},
+        );
+    }
+
+    # where to store the pot file
+    if ( defined $init{pot_dir} ) {
+        $self->_set_pot_dir( delete $init{pot_dir} );
+    }
+
+    # how to store the pot file
+    if ( defined $init{pot_charset} ) {
+        $self->_set_pot_charset( delete $init{pot_charset} );
+    }
+
+    # how to store the pot file
+    if ( ref $init{pot_header} eq 'HASH' ) {
+        $self->_set_pot_header( delete $init{pot_header} );
+    }
+
+    # how write the pot file
+    if ( exists $init{is_append} ) {
+        $self->_set_append( delete $init{is_append} );
+    }
+
+    # error
+    my $keys = join ', ', keys %init;
+    if ($keys) {
+        croak "Unknown parameter: $keys";
+    }
+
+    return $self;
+}
+
+my @names = qw(
+    preprocess_code start_rule rules run_debug parameter_mapping_code
+    pot_dir pot_charset pot_header is_append
+    content_ref references
+);
+
+for my $name (@names) {
+    (my $data_name = $name) =~ s{\A is_}{}xms;
+    no strict qw(refs);       ## no critic (NoStrict)
+    no warnings qw(redefine); ## no critic (NoWarnings)
+
+    *{"_set_$data_name"} = sub {
+        my ($self, $data) = @_;
+
+        $self->{$data_name} = $data;
+
+        return $self;
+    };
+
+    if ($name ne $data_name) {
+        *{"_is_$data_name"} = sub {
+            return shift->{$data_name};
+        };
+    }
+    else {
+        *{"_get_$data_name"} = sub {
+            return shift->{$data_name};
+        };
+    }
+}
+
+sub debug {
+    my ($self, $message) = @_;
+
+    defined $message
+        or return $self->debug('undef');
+    () = print {*STDERR} "\n# $message";
+
+    return $self;
+}
+
+my %debug_switch_of = (
+    ':all' => ~ 0,
+    parser => 2 ** 0,
+    data   => 2 ** 1,
+    file   => 2 ** 2,
+);
+
+sub _debug {
+    my ($self, $group, @messages) = @_;
+
+    my $run_debug = $self->_get_run_debug()
+        or return $self;
+    my $debug = 0;
+    DEBUG: for ( split m{\s+}xms, $run_debug ) {
+        my $switch = $_;
+        my $is_not = $switch =~ s{\A !}{}xms;
+        if ( exists $debug_switch_of{$switch} ) {
+            if ($is_not) {
+                $debug &= ~ $debug_switch_of{$switch};
+            }
+            else {
+                $debug |= $debug_switch_of{$switch};
+            }
+        }
+        else {
+            croak "Unknwon debug switch $_";
+        }
+    }
+    $debug & $debug_switch_of{$group}
+        or return $self;
+
+    for my $line ( map { split m{\n}xms, $_ } @messages ) {
+        $self->debug($line);
+    }
+
+    return $self;
+}
+
+sub _parse_pos {
+    my $self = shift;
+
+    my $regex = $self->_get_start_rule();
+    my $content_ref = $self->_get_content_ref();
+    my @references;
+    while ( ${$content_ref} =~ m{\G .*? ($regex)}xmsgc ) {
+        push @references, {
+            start_pos => pos( ${$content_ref} ) - length $1,
+        };
+    }
+    $self->_set_references(\@references);
+
+    return $self;
+}
+
+sub _parse_rules {
+    my $self = shift;
+
+    my $content_ref = $self->_get_content_ref();
+    for my $reference ( @{ $self->_get_references() } ) {
+        my $rules       = clone $self->_get_rules();
+        my $pos         = $reference->{start_pos};
+        my $has_matched = 0;
+        $self->_debug('parser', "Starting at pos $pos.");
+        my (@parent_rules, @parent_pos);
+        RULE: {
+            my $rule = shift @{$rules};
+            if (! $rule) {
+                $self->_debug('parser', 'No more rules found.');
+                if (@parent_rules) {
+                    $rules = pop @parent_rules;
+                    ()     = pop @parent_pos;
+                    $self->_debug('parser', 'Going back to parent.');
+                    redo RULE;
+                }
+                last RULE;
+            }
+            # goto child
+            if ( ref $rule eq 'ARRAY' ) {
+                push @parent_rules, $rules;
+                push @parent_pos,   $pos;
+                $rules = clone $rule;
+                $self->_debug('parser', 'Going to child.');
+                redo RULE;
+            }
+            # alternative
+            if ( $rule eq 'OR' ) {
+                if ( $has_matched ) {
+                    $rules       = pop @parent_rules;
+                    ()           = pop @parent_pos;
+                    $has_matched = 0;
+                    $self->_debug('parser', 'Ignore alternative.');
+                    redo RULE;
+                }
+                $self->_debug('parser', 'Try alternative.');
+                redo RULE;
+            }
+            pos ${ $content_ref } = $pos;
+            $self->_debug('parser', "Set the current pos to $pos.");
+            $has_matched
+                = my ($match, @result)
+                = ${$content_ref} =~ m{\G ($rule)}xms;
+            if ($has_matched) {
+                push @{ $reference->{parameter} }, @result;
+                $pos += length $match;
+                $self->_debug(
+                    'parser',
+                    qq{Rule $rule has matched:},
+                    ( split m{\n}xms, $match ),
+                    "The current pos is $pos.",
+                );
+                redo RULE;
+            }
+            $rules = pop @parent_rules;
+            $pos   = pop @parent_pos;
+            $self->_debug(
+                'parser',
+                "Rule $rule has not matched.",
+                'Going back to parent.',
+            );
+            redo RULE;
+        }
+    }
+
+    return $self;
+}
+
+sub _cleanup {
+    my $self = shift;
+
+    my $references = $self->_get_references();
+    my $index = 0;
+    @{$references} = grep {
+        exists $_->{parameter}
+    } @{$references};
+
+    return $self;
+}
+
+sub _calculate_reference {
+    my $self = shift;
+
+    my $content_ref = $self->_get_content_ref();
+    for my $reference ( @{ $self->_get_references() } ) {
+        my $pre_match = substr ${$content_ref}, 0, $reference->{start_pos};
+        my $newline_count = $pre_match =~ tr{\n}{\n};
+        $reference->{line_number} = $newline_count + 1;
+    }
+
+    return $self;
+}
+
+sub _calculate_pot_data {
+    my ($self, $file_name) = @_;
+
+    if ( $self->_get_run_debug() ) {
+        require Data::Dumper;
+        $self->_debug(
+            'data',
+            Data::Dumper
+                ->new([$self->_get_references()], [qw(parameters)])
+                ->Sortkeys(1)
+                ->Dump()
+        );
+    }
+    my $parameter_mapping_code = $self->_get_parameter_mapping_code();
+    REFERENCE:
+    for my $reference ( @{ $self->_get_references() } ) {
+        my $parameter = $parameter_mapping_code->(
+            delete $reference->{parameter},
+        ) or next REFERENCE;
+        $reference->{pot_data} = {(
+            reference => "$file_name:$reference->{line_number}",
+            %{$parameter},
+        )};
+    }
+
+    return $self;
+}
+
+sub _store_pot_file {
+    my ($self, $file_name) = @_;
+
+    # create a new pot file
+    my $dbh = DBI->connect(
+        'DBI:PO:'
+        . (
+            join q{;}, (
+                (
+                    defined $self->_get_pot_dir()
+                    ? join q{=}, 'f_dir', $self->_get_pot_dir()
+                    : ()
+                ),
+                (
+                    defined $self->_get_pot_charset()
+                    ? join q{=}, 'po_charset', $self->_get_pot_charset()
+                    : ()
+                ),
+            )
+        ),
+        undef,
+        undef,
+        {RaiseError => 1},
+    );
+    $dbh->{po_tables}->{pot} = {file => "$file_name.pot"};
+    if (! $self->_is_append()) {
+        $dbh->do('DROP TABLE IF EXISTS pot');
+    }
+    if (! -f "$self->_get_pot_dir()/$file_name.pot") {
+        $dbh->do(<<'EO_SQL');
+            CREATE TABLE pot (
+                reference    VARCHAR,
+                msgctxt      VARCHAR,
+                msgid        VARCHAR,
+                msgid_plural VARCHAR
+            )
+EO_SQL
+    }
+
+    # write the header
+    $self->_debug('file', "Write header of $file_name.pot");
+    my $header_msgstr = $dbh->func(
+        {(
+            'Plural-Forms' => 'nplurals=2; plural=n != 1;',
+            %{ $self->_get_pot_header() || {} },
+        )},
+        'build_header_msgstr',
+    );
+    $dbh->do(<<'EO_SQL', undef, $header_msgstr);
+        INSERT INTO pot
+        (msgstr)
+        VALUES (?)
+EO_SQL
+
+    # to check if the entry is known
+    my $sth_select = $dbh->prepare(<<'EO_SQL');
+        SELECT reference
+        FROM pot
+        WHERE
+            msgctxt=?
+            AND msgid=?
+            AND msgid_plural=?
+EO_SQL
+
+    # to insert a new entry
+    my $sth_insert = $dbh->prepare(<<'EO_SQL');
+        INSERT INTO pot
+        (reference, msgctxt, msgid, msgid_plural)
+        VALUES (?, ?, ?, ?)
+EO_SQL
+
+    # to add the next reference to a known entry
+    my $sth_update = $dbh->prepare(<<'EO_SQL');
+        UPDATE pot
+        SET reference=?
+        WHERE
+            msgctxt=?
+            AND msgid=?
+            AND msgid_plural=?
+EO_SQL
+
+    # write entrys
+    REFERENCE:
+    for ( @{ $self->_get_references() } ) {
+        my $entry = $_->{pot_data}
+            or next REFERENCE;
+        $sth_select->execute(
+            map {
+                defined $_ ? $_ : q{};
+            } @{$entry}{ qw(msgctxt msgid msgid_plural) }
+        );
+        my ($reference) = $sth_select->fetchrow_array();
+        if ($reference && length $reference) {
+            # Concat with the po_separator. The default is "\n".
+            $reference = "$reference\n$entry->{reference}";
+            $self->_debug(
+                'file',
+                "Data found, update reference to $reference",
+            );
+            $sth_update->execute(
+                $reference,
+                map {
+                    defined $_ ? $_ : q{};
+                } @{$entry}{ qw(msgctxt msgid msgid_plural) }
+            );
+        }
+        else {
+            $self->_debug(
+                'file',
+                "Data not found, insert reference $entry->{reference}",
+            );
+            $sth_insert->execute(
+                @{$entry}{ qw(reference msgctxt msgid msgid_plural) },
+            );
+        }
+    }
+
+    # all finished
+    for ($sth_select, $sth_insert, $sth_update) {
+        $_->finish();
+    }
+    $dbh->disconnect();
+
+    return $self;
+}
+
+sub extract {
+    my ($self, $file_name, $file_handle) = @_;
+
+    defined $file_name
+        or croak 'No file name given';
+    if (! ref $file_handle) {
+        open $file_handle, '<', $file_name ## no critic (BriefOpen)
+            or croak "Can not open file $file_name\n$OS_ERROR";
+    }
+
+    local $INPUT_RECORD_SEPARATOR = ();
+    $self->_set_content_ref(\<$file_handle>);
+    () = close $file_handle;
+
+    if ( $self->_get_preprocess_code() ) {
+        $self->_get_preprocess_code()->( $self->_get_content_ref() );
+    }
+    $self->_parse_pos();
+    $self->_parse_rules();
+    $self->_cleanup();
+    $self->_calculate_reference();
+    $self->_calculate_pot_data($file_name);
+    $self->_store_pot_file($file_name);
+
+    return $self;
+}
+
+1;
+
+__END__
+
+=head1 NAME
+
+Locale::TextDomain::OO::Extract - Extracts internationalization data as gettext pot file
+
+$Id: Extract.pm 271 2010-01-16 07:37:06Z steffenw $
+
+$HeadURL: https://perl-gettext-oo.svn.sourceforge.net/svnroot/perl-gettext-oo/module/trunk/lib/Locale/TextDomain/OO/Extract.pm $
+
+=head1 VERSION
+
+0.05
+
+=head1 DESCRIPTION
+
+This module extracts internationalizations data and stores this in a pot file.
+
+=head1 SYNOPSIS
+
+    use Locale::TextDomain::OO::Extract;
+
+=head1 SUBROUTINES/METHODS
+
+=head2 method init
+
+This method is for initializing DBD::PO.
+How to initialize see L<DBD::PO>.
+
+    BEGIN {
+        Locale::TextDomain::OO::Extract->init( qw(:plural) );
+    }
+
+=head2 method new
+
+All parameters are optional.
+
+    my $extractor = Locale::TextDomain::OO::Extract->new(
+        # prepare the file and the encoding
+        preprocess_code => sub {
+            my $content_ref = shift;
+
+            ...
+
+            return;
+        },
+
+        # how to find such lines
+        start_rule => qr{__ n?p?x? \(}xms
+
+        # how to find the parameters
+        rules => [
+            [
+                # __( 'text'
+                # __x( 'text'
+                qr{__ (x?) \s* \( \s*}xms,
+                qr{\s*}xms,
+                # You can re-use the next reference.
+                # It is a subdefinition.
+                [
+                    qr{'}xms,
+                    qr{( (?: \\\\ \\\\ | \\\\ ' | [^'] )+ )}xms,
+                    qr{'}xms,
+                ],
+            ],
+            # The next array reference describes an alternative
+            # and not a subdefinition.
+            'OR',
+            [
+                # next alternative e.g.
+                # __n( 'context' , 'text'
+                # __nx( 'context' , 'text'
+                ...
+            ],
+        ],
+
+        # debug output for other rules than perl
+        run_debug => ':all !parser', # debug all but not the parser
+                     # :all    - switch on all debugs
+                     # parser  - switch on parser debug
+                     # data    - switch on data debug
+                     # file    - switch on file debug
+                     # !parser - switch off parser debug
+                     # !data   - switch off data debug
+                     # !file   - switch off file debug
+
+        # how to map the parameters to pot file
+        parameter_mapping_code => sub {
+            my $parameter = shift;
+
+            # The chars after __ were stored to make a decision now.
+            my $context_parameter = shift @{$parameter};
+
+            return {
+                msgctxt      => $context_parameter =~ m{p}xms
+                                ? $context_parameter
+                                : undef,
+                msgid        => scalar shift @{$parameter},
+                msgid_plural => scalar shift @{$parameter},
+            };
+        },
+
+        # where to store the pot file
+        pot_dir => './',
+
+        # how to store the pot file
+        # - The meaning of undef is ISO-8859-1 but use not Perl unicode.
+        # - Set 'ISO-8859-1' to have a ISO-8859-1 pot file and use Perl unicode.
+        # - Set 'UTF-8' to have a UTF-8 pot file and use Perl unicode.
+        # And so on.
+        pot_charset => undef,
+
+        # add some key value pairs to the header
+        # more see documentation of DBD::PO
+        pot_header => { ... },
+
+        # how to write the pot file
+        is_append => $boolean,
+    );
+
+=head2 method extract
+
+The default pot_dir is "./".
+
+Call
+
+    $extractor->extract('dir/filename.pl');
+
+to extract "dir/filename.pl" to have a "$pot_dir/dir/filename.pl.pot".
+
+Call
+
+    open my $file_handle, '<', 'dir/filename.pl'
+        or croak "Can no open file dir/filename.pl\n$OS_ERROR";
+    $extractor->extract('filename', $file_handle);
+
+to extract "dir/filename.pl" to have a "$pot_dir/filename.pot".
+
+=head2 method debug
+
+Switch on the debug to see on STDERR how the rules are handled.
+Inherit of this class and write your own debug method if needed.
+
+=head1 EXAMPLE
+
+Inside of this distribution is a directory named example.
+Run this *.pl files.
+
+=head1 DIAGNOSTICS
+
+Error message in case of unknown parameters at method new.
+
+ Unknown parameter: ...
+
+Undef is not a filename.
+
+ No file name given
+
+There is a problem in opening the file to extract.
+
+ Can not open file ...
+
+=head1 CONFIGURATION AND ENVIRONMENT
+
+none
+
+=head1 DEPENDENCIES
+
+version
+
+Carp
+
+English
+
+Clone
+
+DBI
+
+DBD::PO
+
+=head2 dynamic require
+
+L<Data::Dumper>
+
+=head1 INCOMPATIBILITIES
+
+not known
+
+=head1 BUGS AND LIMITATIONS
+
+none
+
+=head1 SEE ALSO
+
+L<Locale::TextDoamin::OO>
+
+=head1 AUTHOR
+
+Steffen Winkler
+
+=head1 LICENSE AND COPYRIGHT
+
+Copyright (c) 2009,
+Steffen Winkler
+C<< <steffenw at cpan.org> >>.
+All rights reserved.
+
+This module is free software;
+you can redistribute it and/or modify it
+under the same terms as Perl itself.
+
+=cut
---
+++ extract/branches/moose/lib/Locale/TextDomain/OO/Extract/Perl.pm
@@ -0,0 +1,251 @@
+package Locale::TextDomain::OO::Extract::Perl;
+
+use strict;
+use warnings;
+
+use version; our $VERSION = qv('0.05');
+
+use parent qw(Locale::TextDomain::OO::Extract);
+
+my $context_rule
+    = my $text_rule
+    = my $singular_rule
+    = my $plural_rule
+    = [
+        qr{'}xms,
+        qr{( (?: \\\\ \\\\ | \\\\ ' | [^'] )+ )}xms,
+        qr{'}xms,
+    ];
+
+my $komma_rule = qr{\s* , \s*}xms;
+
+my $start_rule = qr{
+    (?:
+        __ n?p?x?
+        |
+        maketext (?: _p )?
+    )
+    \s*
+    \(
+}xms;
+
+my $rules = [
+    [
+        qr{__ (x?) \s* \( \s*}xms,
+        $text_rule,
+    ],
+    'OR',
+    [
+        qr{__ (nx?) \s* \( \s*}xms,
+        $singular_rule,
+        $komma_rule,
+        $plural_rule,
+    ],
+    'OR',
+    [
+        qr{__ (px?) \s* \( \s*}xms,
+        $context_rule,
+        $komma_rule,
+        $text_rule,
+    ],
+    'OR',
+    [
+        qr{__ (npx?) \s* \( \s*}xms,
+        $context_rule,
+        $komma_rule,
+        $singular_rule,
+        $komma_rule,
+        $plural_rule,
+    ],
+    'OR',
+    [
+        qr{maketext () \s* \( \s*}xms,
+        $text_rule,
+    ],
+    'OR',
+    [
+        qr{maketext_ (p) \s* \( \s*}xms,
+        $context_rule,
+        $komma_rule,
+        $text_rule,
+    ],
+];
+
+my $remove_pod_code = sub {
+    my $content_ref = shift;
+
+    my ($is_pod, $is_end);
+    ${$content_ref} = join "\n", map {
+        $_ eq '__END__'  ? do { $is_end = 1; q{} }
+        : $is_end        ? ()
+        : m{= (\w+)}xms  ? (
+            lc $1 eq 'cut'
+            ? do { $is_pod = 0; q{} }
+            : do { $is_pod = 1; q{} }
+        )
+        : $is_pod        ? q{}
+        : $_;
+    } split m{\r? \n \r?}xms, ${$content_ref};
+
+    return;
+};
+
+my $parameter_mapping_code = sub {
+    my $parameter = shift;
+
+    my $extra_parameter = shift @{$parameter};
+    @{$parameter}
+        or return;
+
+    return {
+        msgctxt      => $extra_parameter =~ m{p}xms
+                        ? scalar shift @{$parameter}
+                        : undef,
+        msgid        => scalar shift @{$parameter},
+        msgid_plural => scalar shift @{$parameter},
+    };
+};
+
+sub new {
+    my ($class, %init) = @_;
+
+    return $class->SUPER::new(
+        preprocess_code        => $remove_pod_code,
+        start_rule             => $start_rule,
+        rules                  => $rules,
+        parameter_mapping_code => $parameter_mapping_code,
+        %init,
+    );
+}
+
+1;
+
+__END__
+
+=head1 NAME
+
+Locale::TextDomain::OO::Extract::Perl
+- Extracts internationalization data from Perl source code
+
+$Id: Perl.pm 271 2010-01-16 07:37:06Z steffenw $
+
+$HeadURL: https://perl-gettext-oo.svn.sourceforge.net/svnroot/perl-gettext-oo/module/trunk/lib/Locale/TextDomain/OO/Extract/Perl.pm $
+
+=head1 VERSION
+
+0.04
+
+=head1 DESCRIPTION
+
+This module extracts internationalization data from Perl source code.
+
+Implemented rules:
+
+ __('...
+ __x('...
+ __n('...
+ __nx('...
+ __p('...
+ __px('...
+ __np('...
+ __npx('...
+ maketext('...
+ maketext_p('...
+
+Anything before __ is allowed, e.g. N__ and so on.
+Whitespace is allowed everywhere.
+
+=head1 SYNOPSIS
+
+    use Locale::TextDomain::OO::Extract::Perl;
+
+=head1 SUBROUTINES/METHODS
+
+=head2 method new
+
+All parameters are optional.
+See Locale::TextDomain::OO::Extract to replace the defaults.
+
+    my $extractor = Locale::TextDomain::OO::Extract::Perl->new(
+        # where to store the pot file
+        pot_dir => './',
+
+        # how to store the pot file
+        # - The meaning of undef is ISO-8859-1 but use not Perl unicode.
+        # - Set 'ISO-8859-1' to have a ISO-8859-1 pot file and use Perl unicode.
+        # - Set 'UTF-8' to have a UTF-8 pot file and use Perl unicode.
+        # And so on.
+        pot_charset => undef,
+
+        # add some key value pairs to the header
+        # more see documentation of DBD::PO
+        pot_header => { ... },
+    );
+
+=head2 method extract
+
+The default pot_dir is "./".
+
+Call
+
+    $extractor->extract('dir/filename.pl');
+
+to extract "dir/filename.pl" to have a "$pot_dir/dir/filename.pl.pot".
+
+Call
+
+    open my $file_handle, '<', 'dir/filename.pl'
+        or croak "Can no open file dir/filename.pl\n$OS_ERROR";
+    $extractor->extract('filename', $file_handle);
+
+to extract "dir/filename.pl" to have a "$pot_dir/filename.pot".
+
+=head1 EXAMPLE
+
+Inside of this distribution is a directory named example.
+Run this *.pl files.
+
+=head1 DIAGNOSTICS
+
+see Locale::TextDomain::OO::Extract
+
+=head1 CONFIGURATION AND ENVIRONMENT
+
+none
+
+=head1 DEPENDENCIES
+
+version
+
+parent
+
+L<Locale::TextDomain::OO::Extract>
+
+=head1 INCOMPATIBILITIES
+
+not known
+
+=head1 BUGS AND LIMITATIONS
+
+none
+
+=head1 SEE ALSO
+
+L<Locale::TextDoamin::OO>
+
+=head1 AUTHOR
+
+Steffen Winkler
+
+=head1 LICENSE AND COPYRIGHT
+
+Copyright (c) 2009,
+Steffen Winkler
+C<< <steffenw at cpan.org> >>.
+All rights reserved.
+
+This module is free software;
+you can redistribute it and/or modify it
+under the same terms as Perl itself.
+
+=cut
