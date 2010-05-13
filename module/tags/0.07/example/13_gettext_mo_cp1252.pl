2d18e66dafd0a23c2a7ecb88567e88c992ab06ce
steffenw <steffenw@2090c879-ba7a-46ae-a66c-a00c6d59768f>
2010-05-13 08:33:10 +0000

    tag version 0.07
    git-svn-id: http://svn.code.sf.net/p/perl-gettext-oo/code@358 2090c879-ba7a-46ae-a66c-a00c6d59768f

---
+++ module/tags/0.07/example/13_gettext_mo_cp1252.pl
@@ -0,0 +1,47 @@
+#!perl -T
+
+use strict;
+use warnings;
+use utf8;
+
+our $VERSION = 0;
+
+use Carp qw(croak);
+use English qw(-no_match_vars $OS_ERROR);
+use Encode qw(encode decode);
+require Locale::TextDomain::OO;
+
+local $ENV{LANG} = (); # for author test only
+local $ENV{LANGUAGE}
+    = Locale::TextDomain::OO
+    ->get_default_language_detect()
+    ->('de');
+my $text_domain = 'example_cp1252';
+my $encoding    = 'cp1252';
+
+my $loc = Locale::TextDomain::OO->new(
+    text_domain  => $text_domain,
+    search_dirs  => [qw(./LocaleData/)],
+    # input filter
+    input_filter => sub { encode($encoding, shift) },
+    # output filter
+    filter       => sub { decode($encoding, shift) },
+);
+
+# all unicode chars encode to UTF-8
+binmode STDOUT, ':encoding(utf-8)'
+    or croak "Binmode STDOUT\n$OS_ERROR";
+
+# run all translations
+() = print map {"$_\n"}
+    $loc->__(
+        'This are German umlauts: ä ö ü ß Ä Ö Ü.',
+    );
+
+# $Id$
+
+__END__
+
+Output:
+
+Das sind deutsche Umlaute: ä ö ü ß Ä Ö Ü.
---
+++ module/tags/0.07/example/14_N__.pl
@@ -0,0 +1,108 @@
+#!perl -T
+
+use strict;
+use warnings;
+
+our $VERSION = 0;
+
+require Locale::TextDomain::OO;
+
+local $ENV{LANGUAGE}
+    = Locale::TextDomain::OO
+    ->get_default_language_detect()
+    ->('de_DE');
+my $text_domain = 'example';
+
+my $loc = Locale::TextDomain::OO->new(
+    text_domain => $text_domain,
+    search_dirs => [qw(./LocaleData/)],
+);
+
+# Put all data for the translation into a structure.
+# That allows the extractor to find all the phrases.
+my @extractable_data = (
+    q{} => [
+        $loc->N__(
+            'This is a text.',
+        )
+    ],
+    x => [
+        $loc->N__x(
+            '{name} is programming {language}.',
+            name     => 'Steffen',
+            language => 'Perl',
+        )
+    ],
+    n => [
+        $loc->N__n(
+            'Singular',
+            'Plural',
+            1,
+        )
+    ],
+    nx => [
+        $loc->N__nx(
+            '{num} shelf',
+            '{num} shelves',
+            1,
+            num => 1,
+        )
+    ],
+    p => [
+        $loc->N__p(
+            'maskulin',
+            'Dear',
+        )
+    ],
+    px => [
+        $loc->N__px(
+            'maskulin',
+            'Dear {name}',
+            name => 'Winkler',
+        )
+    ],
+    np => [
+        $loc->N__np(
+            'better',
+            'shelf',
+            'shelves',
+            1,
+        )
+    ],
+    npx => [
+        $loc->N__npx(
+            'better',
+            '{num} shelf',
+            '{num} shelves',
+            1,
+            num => 1,
+        )
+    ],
+);
+
+# Do any complex things and run the translations later.
+while (my ($method_suffix, $array_ref) = splice @extractable_data, 0, 2) {
+    my $method = "__$method_suffix";
+    () = print
+        $loc->$method( @{$array_ref} ),
+        "\n";
+}
+
+# $Id$
+
+__END__
+
+Output:
+
+Das ist ein Text.
+Steffen programmiert Perl.
+Einzahl
+Mehrzahl
+1 Regal
+2 Regale
+Sehr geehrter Herr
+Sehr geehrter Herr Winkler
+gutes Regal
+gute Regale
+1 gutes Regal
+2 gute Regale
