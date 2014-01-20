d187afdbceca5fb86ac90e7e1108e0fc1403cf7c
steffenw <steffenw@2090c879-ba7a-46ae-a66c-a00c6d59768f>
2014-01-20 21:09:21 +0000

    prepare 1.007
    git-svn-id: http://svn.code.sf.net/p/perl-gettext-oo/code@468 2090c879-ba7a-46ae-a66c-a00c6d59768f

--- module/trunk/lib/Locale/TextDomain/OO.pm
+++ module/trunk/lib/Locale/TextDomain/OO.pm
@@ -3,7 +3,7 @@ package Locale::TextDomain::OO; ## no critic (TidyCode)
 use strict;
 use warnings;
 
-our $VERSION = '1.006';
+our $VERSION = '1.007';
 
 use Locale::TextDomain::OO::Translator;
 
@@ -29,7 +29,7 @@ $HeadURL$
 
 =head1 VERSION
 
-1.006
+1.007
 
 Starting with version 1.000 the interface has changed.
 
