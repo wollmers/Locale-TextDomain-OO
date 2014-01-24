dde0b0451772ff164df54dc67adf7d351ab8ed33
steffenw <steffenw@2090c879-ba7a-46ae-a66c-a00c6d59768f>
2014-01-24 12:04:08 +0000

    tag 0.005
    git-svn-id: http://svn.code.sf.net/p/perl-gettext-oo/code@477 2090c879-ba7a-46ae-a66c-a00c6d59768f

---
+++ Locale-Utils-PlaceholderNamed/tags/0.005/lib/Locale/Utils/PlaceholderNamed.pm
@@ -0,0 +1,215 @@
+package Locale::Utils::PlaceholderNamed; ## no critic (TidyCode)
+
+use strict;
+use warnings;
+use Carp qw(confess);
+use Moo;
+use MooX::StrictConstructor;
+use MooX::Types::MooseLike::Base qw(Bool CodeRef);
+use namespace::autoclean;
+
+our $VERSION = '0.005';
+
+has strict => (
+    is  => 'rw',
+    isa => Bool,
+);
+
+has modifier_code => (
+    is      => 'rw',
+    isa     => CodeRef,
+    clearer => 'clear_modifier_code',
+);
+
+sub _mangle_value {
+    my ($self, $placeholder, $value, $attribute) = @_;
+
+    defined $value
+        or return $self->strict ? $placeholder : q{};
+    defined $attribute
+        or return $value;
+    $self->modifier_code
+        or return $value;
+    $value = $self->modifier_code->($value, $attribute);
+    defined $value
+        or confess 'modifier_code returns nothing or undef';
+
+    return $value;
+}
+
+sub expand_named {
+    my ($self, $text, @args) = @_;
+
+    defined $text
+        or return $text;
+    my $arg_ref = @args == 1
+        ? $args[0]
+        : {
+            @args % 2
+            ? confess 'Arguments expected pairwise'
+            : @args
+        };
+
+    my $regex = join q{|}, map { quotemeta $_ } keys %{$arg_ref};
+    ## no critic (EscapedMetacharacters)
+    $text =~ s{
+        (
+            \{
+            ( $regex )
+            (?: [ ]* [:] ( [^:\}]+ ) )?
+            \}
+        )
+    }
+    {
+        $self->_mangle_value($1, $arg_ref->{$2}, $3)
+    }xmsge;
+    ## use critic (EscapedMetacharacters)
+
+    return $text;
+}
+
+__PACKAGE__->meta->make_immutable;
+
+1;
+
+__END__
+
+=head1 NAME
+
+Locale::Utils::PlaceholderNamed - Utils to expand named placeholders
+
+$Id$
+
+$HeadURL$
+
+=head1 VERSION
+
+0.005
+
+=head1 SYNOPSIS
+
+    use Locale::Utils::PlaceholderNamed;
+
+    my $obj = Locale::Utils::PlaceholderNamed->new(
+        # optional strict switch
+        strict => 1,
+        # optional modifier code
+        modifier_code => sub {
+            my ( $value, $attribute ) = @_;
+            return
+                $attribute eq '%.3f'
+                ? sprintf($attribute, $value)
+                : $attribute eq 'accusative'
+                ? accusative($value)
+                : $value;
+        },
+    );
+
+    $expanded = $obj->expand_named($text, %args);
+    $expanded = $obj->expand_named($text, \%args);
+
+=head1 DESCRIPTION
+
+Utils to expand named placeholders.
+
+=head1 SUBROUTINES/METHODS
+
+=head2 method strict
+
+If strict is false: undef will be converted to q{}.
+If strict is true: no replacement.
+
+    $obj->strict(1); # boolean true or false;
+
+=head2 method modifier_code, clear_modifier_code
+
+The modifier code handles named attributes
+to modify the given placeholder value.
+
+If the placeholder name is C<{foo:bar}> then foo is the placeholder name
+and bar the attribute name.
+Space in front of the attribute name is allowed, e.g. C<{foo :bar}>.
+
+    my $code_ref = sub {
+        my ( $value, $attribute ) = @_;
+        return
+            $attribute eq 'num.03'
+            ? sprintf('%.03f, $value)
+            : $attribute eq 'accusative'
+            ? accusative($value)
+            : $value;
+    };
+    $obj->modifier_code($code_ref);
+
+To switch off this code - clear them.
+
+    $obj->clear_modifier_code;
+
+=head2 method expand_named
+
+Expands strings containing named placeholders like C<{name}>.
+
+    $text = 'foo {name} baz';
+    %args = (
+        name => 'bar',
+    );
+
+    $expanded = $obj->expand_named($text, %args);
+
+or
+
+    $expanded = $obj->expand_text($text, \%args);
+
+=head1 EXAMPLE
+
+Inside of this distribution is a directory named example.
+Run the *.pl files.
+
+=head1 DIAGNOSTICS
+
+none
+
+=head1 CONFIGURATION AND ENVIRONMENT
+
+none
+
+=head1 DEPENDENCIES
+
+L<Carp|Carp>
+
+L<Moo|Moo>
+
+L<MooX::StrictConstructor|MooX::StrictConstructor>
+
+L<MooX::Types::MooseLike|MooX::Types::MooseLike>
+
+L<namespace::autoclean|namespace::autoclean>
+
+=head1 INCOMPATIBILITIES
+
+not known
+
+=head1 BUGS AND LIMITATIONS
+
+not known
+
+=head1 SEE ALSO
+
+L<http://en.wikipedia.org/wiki/Gettext>
+
+L<Locale::TextDomain|Locale::TextDomain>
+
+=head1 AUTHOR
+
+Steffen Winkler
+
+=head1 LICENSE AND COPYRIGHT
+
+Copyright (c) 2011 - 2014,
+Steffen Winkler
+C<< <steffenw at cpan.org> >>.
+All rights reserved.
+
+This module is free software;
+you can redistribute it and/or modify it
+under the same terms as Perl itself.
---
+++ Locale-Utils-PlaceholderNamed/tags/0.005/t/21_test_examples.t
@@ -0,0 +1,60 @@
+#!perl
+
+use strict;
+use warnings;
+
+use Test::More;
+use Test::Differences;
+use Cwd qw(getcwd chdir);
+
+$ENV{AUTHOR_TESTING}
+    or plan skip_all => 'Set $ENV{AUTHOR_TESTING} to run this test.';
+
+my @data = (
+    {
+        test   => '01_expand_named',
+        path   => 'example',
+        script => '-I../lib -T 01_expand_named.pl',
+        result => <<'EOT',
+{count} EUR
+0 EUR
+1 EUR
+2 EUR
+345.678,90 EUR
+45.678,9 EUR
+ EUR
+0 EUR
+1 EUR
+2 EUR
+345678.90 EUR
+45678.9 EUR
+EOT
+    },
+    {
+        test   => '02_modifier_code',
+        path   => 'example',
+        script => '-I../lib -T 02_modifier_code.pl',
+        result => <<'EOT',
+ EUR
+0 EUR
+1 EUR
+2 EUR
+345.678,90 EUR
+45.678,9 EUR
+EOT
+    },
+);
+
+plan tests => 0 + @data;
+
+for my $data (@data) {
+    my $dir = getcwd;
+    chdir("$dir/$data->{path}");
+    my $result = qx{perl $data->{script} 2>&3};
+    chdir($dir);
+    eq_or_diff(
+        $result,
+        $data->{result},
+        $data->{test},
+    );
+}
